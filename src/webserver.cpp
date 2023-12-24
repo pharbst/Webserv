db010a28c0534d54172b73ab88e1f48eff9a9558
Peter Harbst <pharbst@2-F-7.42heilbronn.de>
2023-12-24 16:34:34 +0100

    Init:	init commit 	->added Makefile 	->added color.mk 	->simple structure

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -1,10 +1,14 @@
 #include "httpServer.hpp"
+#include "Test.hpp"
 
 //int socket(int domain, int type, int protocol);
 // 'IPv4' defined by 'AF_INET', 'SOCK_STREAM' for bidirectional TCP traffic, '0'=default protocol for type
 
 int main()
 {
+	Test test;
+	test.setTest("Hello!");
+
 	httpServer serv = httpServer("0.0.0.0", 8080);
 	serv.startServer();
 	serv.startListen();
