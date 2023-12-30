c57e1c6adaa2e5ebf3694efd7f5786b0c1237fca
Peter Harbst <pharbst@2-F-7.42heilbronn.de>
2023-12-30 14:03:35 +0100

    Wip:	creating sockets and start listening works

--- include/socketManager.hpp
+++ include/socketManager.hpp
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 18:31:42 by pharbst           #+#    #+#             */
-/*   Updated: 2023/12/25 20:48:25 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/12/30 13:55:48 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,19 @@ class socketManager {
 	public:
 		// public methods
 		/**
-		 * @brief Create a Socket object
-		 * 
-		 * @param port port to listen on
-		 * @param ipVersion IPV4, IPV6 or LOCALHOST (to use both ip versions use IPV4 + IPV6)
-		 * @param protocol protocol to use for client sockets (TCP or UDP) the listening socket will always use TCP
-		 */
-		static void	createSocket(uint32_t port, uint8_t ipVersion, uint8_t protocol);
+		* @brief Create a Socket object
+		* 
+		* @param port port to listen on
+		* @param ipVersion IPV4, IPV6 or LOCALHOST (to use both ip versions use IPV4 + IPV6)
+		* @param protocol protocol to use for client sockets (TCP or UDP) the listening socket will always use TCP
+		*/
+		static void	createSocket(uint32_t port, uint32_t ipVersion, uint8_t protocol);
 		static void	startListening(uint32_t port);
 		static const std::map<int, t_socket>::iterator getSockets(int port);
 
 	private:
 		// private methods
-		static int	bindSocket(int socket, struct sockaddr* addr);
+		static int	bindSocket(int socket, struct sockaddr* addr, socklen_t len);
 		static int addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock);
 		// port, sockets
 		static std::map<int, t_socket>		_sockets;
--- src/socketManager.cpp
+++ src/socketManager.cpp
@@ -6,18 +6,40 @@
 /*   By: pharbst <pharbst@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 16:49:04 by pharbst           #+#    #+#             */
-/*   Updated: 2023/12/25 20:50:35 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/12/30 13:57:48 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "socketManager.hpp"
 
 std::map<int, t_socket>			socketManager::_sockets;
 std::map<int, int>				socketManager::_clientSockets;
 
-void	socketManager::createSocket(uint32_t port, uint8_t ipVersion, uint8_t protocol) {
+void	socketManager::createSocket(uint32_t port, uint32_t ipVersion, uint8_t protocol) {
 	(void)protocol;
+	// uint32_t	ipVersioncp = ipVersion;
+	// debugging info
+	// {
+	// 	std::cout << "createSocket called with: " << std::endl;
+	// 	std::cout << "port: " << port << std::endl;
+	// 	std::cout << "interface: " << std::endl;
+	// 	std::cout << "\tIPV6\t";
+	// 	if (ipVersioncp / IPV6 == 1) {
+	// 		std::cout << "\t\033[1;32m[\xE2\x9C\x94]\033[0m" << std::endl;
+	// 		ipVersioncp -= IPV6;
+	// 	}
+	// 	std::cout << "\tIPV4\t";
+	// 	if (ipVersioncp / IPV4 == 1) {
+	// 		std::cout << "\t\033[1;32m[\xE2\x9C\x94]\033[0m" << std::endl;
+	// 		ipVersioncp -= IPV4;
+	// 	}
+	// 	std::cout << "\tLOCALHOST\t";
+	// 	if (ipVersioncp / LOCALHOST == 1) {
+	// 		std::cout << "\033[1;32m[\xE2\x9C\x94]\033[0m" << std::endl;
+	// 		ipVersioncp -= LOCALHOST;
+	// 	}
+	// 	std::cout << std::endl << std::endl << std::endl;
+	// }
 	if (port > 0XFFFF) {
 		std::cout << "Port is out of range" << std::endl;
 		return ;
@@ -31,33 +53,35 @@ void	socketManager::createSocket(uint32_t port, uint8_t ipVersion, uint8_t proto
 	t_socket sock;
 	std::memset(&sock, 0, sizeof(sock));
 	if (ipVersion / IPV6 == 1) {
-		std::cout << "creating ipv6 socket" << std::endl;
 		ipVersion -= IPV6;
-		if (!addSocket(IPV6, port, sock))
+		if (!socketManager::addSocket(IPV6, port, sock))
 			success++;
 	}
 	if (ipVersion / IPV4 == 1) {
-		std::cout << "creating ipv4 socket" << std::endl;
 		ipVersion -= IPV4;
-		if (!addSocket(IPV4, port, sock))
+		if (!socketManager::addSocket(IPV4, port, sock))
 			success++;
 	}
 	if (ipVersion / LOCALHOST == 1) {
-		std::cout << "creating localhost socket" << std::endl;
 		ipVersion -= LOCALHOST;
-		if (!addSocket(LOCALHOST, port, sock))
+		if (!socketManager::addSocket(LOCALHOST, port, sock))
 			success++;
 	}
 	if (success == 0) {
 		std::cout << "no socket created for port: " << port << std::endl;
 		return ;
 	}
-	std::cout << port << "    " << success << " sockets created" << std::endl;
-	std::cout << std::pair<int, t_socket>(port, sock).first << std::endl;
+	std::cout << success << " socket/s created for port: " << port << std::endl;
 	_sockets.insert(std::pair<int, t_socket>(port, sock));
 }
 
 int	socketManager::addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock) {
+	if (ipVersion == LOCALHOST)
+		std::cout << "creating localhost socket" << std::endl;
+	else if (ipVersion == IPV4)
+		std::cout << "creating ipv4 socket" << std::endl;
+	else if (ipVersion == IPV6)
+		std::cout << "creating ipv6 socket" << std::endl;
 	switch (ipVersion)
 	{
 	case LOCALHOST:
@@ -69,7 +93,7 @@ int	socketManager::addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock) {
 		sock.localAddr.sin_family = LOCALHOST;
 		sock.localAddr.sin_port = htons(port);
 		sock.localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		if (bindSocket(sock.local, (struct sockaddr*)&sock.localAddr)) {
+		if (socketManager::bindSocket(sock.local, (struct sockaddr*)&sock.localAddr, sizeof(sock.localAddr))) {
 			close(sock.local);
 			sock.local = 0;
 			std::memset(&sock.localAddr, 0, sizeof(sock.localAddr));
@@ -85,7 +109,7 @@ int	socketManager::addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock) {
 		sock.ipv4Addr.sin_family = IPV4;
 		sock.ipv4Addr.sin_port = htons(port);
 		sock.ipv4Addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		if (bindSocket(sock.ipv4, (struct sockaddr*)&sock.ipv4Addr)) {
+		if (socketManager::bindSocket(sock.ipv4, (struct sockaddr*)&sock.ipv4Addr, sizeof(sock.ipv4Addr))) {
 			close(sock.ipv4);
 			sock.ipv4 = 0;
 			std::memset(&sock.ipv4Addr, 0, sizeof(sock.ipv4Addr));
@@ -101,7 +125,7 @@ int	socketManager::addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock) {
 		sock.ipv6Addr.sin6_family = IPV6;
 		sock.ipv6Addr.sin6_port = htons(port);
 		sock.ipv6Addr.sin6_addr = in6addr_any;
-		if (bindSocket(sock.ipv6, (struct sockaddr*)&sock.ipv6Addr)) {
+		if (socketManager::bindSocket(sock.ipv6, (struct sockaddr*)&sock.ipv6Addr, sizeof(sock.ipv6Addr))) {
 			close(sock.ipv6);
 			sock.ipv6 = 0;
 			std::memset(&sock.ipv6Addr, 0, sizeof(sock.ipv6Addr));
@@ -114,9 +138,9 @@ int	socketManager::addSocket(uint8_t ipVersion, uint32_t port, t_socket &sock) {
 	}
 }
 
-int	socketManager::bindSocket(int socket, struct sockaddr* addr) {
+int	socketManager::bindSocket(int socket, struct sockaddr* addr, socklen_t len) {
 	std::cout << "Binding socket" << std::endl;
-	if (bind(socket, addr, sizeof(*addr)) < 0) {
+	if (bind(socket, addr, len) < 0) {
 		std::cout << "Failed to bind socket" << std::endl;
 		return -1;
 	}
@@ -151,19 +175,23 @@ void	socketManager::startListening(uint32_t port) {
 			std::cout << "Listening for ipv6 socket failed on port: " << port << std::endl;
 		success += 4;
 	}
-	if (success == 0)
-		std::cout << "No socket listening for port: " << port << std::endl;
-	else {
-		std::cout << "summary: " << std::endl;
-		if (success >> 2 % 2) {
-			std::cout << "ipv6 socket is listening on port: " << port << std::endl;
-			success -= 4;
+	// debugging info
+	std::cout << "debugging: " << std::endl;
+	{
+		if (success == 0)
+			std::cout << "No socket listening for port: " << port << std::endl;
+		else {
+			std::cout << "summary: " << success << std::endl;
+			if ((success >> 2) % 2) {
+				std::cout << "ipv6 socket is listening on port: " << port << std::endl;
+				success -= 4;
+			}
+			if ((success >> 1) % 2) {
+				std::cout << "ipv4 socket is listening on port: " << port << std::endl;
+				success -= 2;
+			}
+			if (success % 2)
+				std::cout << "localhost socket is listening on port: " << port << std::endl;
 		}
-		if (success >> 1 % 2) {
-			std::cout << "ipv4 socket is listening on port: " << port << std::endl;
-			success -= 2;
-		}
-		if (success % 2)
-			std::cout << "localhost socket is listening on port: " << port << std::endl;
 	}
 }
