e82b4d61c7a5154fc929093990085f1f6055415c
Peter Harbst <pharbst@3-B-1.42heilbronn.de>
2024-01-11 00:17:51 +0100

    Add:	raw structure of the weberver programm

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -2,6 +2,38 @@
 
 int main()
 {
-	socketManager::addSocket("127.0.0.1", 8080, IPV4, TCP);
+	int numWorker = 4;
+	// parsing here
+	// add application map to interface before forking the workers
+	applicationInterface::addApplication(80, /*&http::application*/);
+
+	// start workers
+	{
+		for (int i = numWorker; i > 0; i--) {
+			int workerPipe[2];
+			if (pipe(workerPipe) == -1) {
+				std::cout << "Error: Pipe failed" << std::endl;
+				return 1;
+			}
+			int pid = fork();
+			if (pid == -1) {
+				std::cout << "Error: Fork failed" << std::endl;
+				return 1;
+			}
+			if (pid == 0) {
+				close(workerPipe[0]);
+				applicationInterface::Interface(workerPipe[1]);
+				return 0;
+			}
+			close(workerPipe[1]);
+			socketManager::addWorker(pid, workerPipe[0]);
+		}
+	}
+
+	// add sockets
+	{
+		socketManager::addSocket("0.0.0.0", 80, IPV4, TCP);
+	}
+	// socketManager::start();
 	return 0;
 }
